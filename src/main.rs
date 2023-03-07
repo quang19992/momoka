d506db9925f4487fb5d6c9aca8143c29cfa9e4b7
Quang1999 <contact@relay.akimitsu.dev>
2023-03-07 15:29:03 +0700

    feat: load server config from env file

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
+use crate::server_config::ServerConfig;
 use actix_cors::Cors;
-use actix_web::{middleware::Logger, App, HttpServer, web};
+use actix_web::{middleware::Logger, web, App, HttpServer};
 
 mod graphql;
+mod server_config;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    dotenv::dotenv().ok();
+    let config = match ServerConfig::load() {
+        Ok(config) => config,
+        Err(err) => panic!("{:?}", err),
+    };
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
     let schema = std::sync::Arc::new(graphql::schema::create_schema());
 
-    log::info!("starting server on port {}", 8080);
+    log::info!("starting server on port {}", config.http_port);
 
     HttpServer::new(move || {
         App::new()
@@ -18,8 +23,8 @@ async fn main() -> std::io::Result<()> {
             .wrap(Cors::permissive())
             .wrap(Logger::default())
     })
-    .workers(2)
-    .bind(("0.0.0.0", 8080))?
+    .workers(config.num_worker)
+    .bind(("0.0.0.0", config.http_port))?
     .run()
     .await
 }
