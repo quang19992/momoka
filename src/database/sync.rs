a15274732ac4399217f0df564237f012fe8be8ba
Quang1999 <contact@relay.akimitsu.dev>
2023-04-05 03:07:55 +0700

    feat: implement basic synchronizer executor

--- src/database/sync.rs
+++ src/database/sync.rs
@@ -1,4 +1,5 @@
 use super::{bundle::Database, error::DatabaseError};
+use async_recursion::async_recursion;
 use futures::future::BoxFuture;
 use std::{future::Future, result::Result, sync::Arc};
 
@@ -18,7 +19,9 @@ impl SyncFn {
     }
 }
 
-trait SyncSupport {}
+trait SyncSupport {
+    fn execute<T>(query: &str, params: T) -> SyncResponse;
+}
 
 // TODO - implement this
 // this will help in case of a sync failure
@@ -31,11 +34,44 @@ enum Synchronizer<'a> {
 }
 
 impl Synchronizer<'_> {
-    async fn execute<T: SyncSupport>(
-        bundle: Database,
+    #[async_recursion(?Send)]
+    async fn execute<T: SyncSupport + std::marker::Copy>(
+        &self,
+        bundle: Arc<Database>,
         database: T,
         state: Option<SyncState>,
     ) -> SyncResponse {
+        match self {
+            Synchronizer::Simple(queries) => Self::execute_simple_sync(database, queries).await,
+            Synchronizer::Custom(function) => Self::execute_custom_sync(bundle, function).await,
+            Synchronizer::Mixed(synchronizers) => {
+                Self::execute_mixed_sync(synchronizers, bundle, database, state).await
+            }
+        }
+    }
+
+    async fn execute_simple_sync<'a, T: SyncSupport>(
+        database: T,
+        queries: &'a [&'a str],
+    ) -> SyncResponse {
         todo!()
+    }
+
+    async fn execute_custom_sync<'a>(bundle: Arc<Database>, function: &'a SyncFn) -> SyncResponse {
+        todo!()
+    }
+
+    async fn execute_mixed_sync<'a, T: SyncSupport + std::marker::Copy>(
+        synchronizers: &'a [&'a Self],
+        bundle: Arc<Database>,
+        database: T,
+        _state: Option<SyncState>,
+    ) -> SyncResponse {
+        for synchronizer in synchronizers.iter() {
+            if let Err(err) = synchronizer.execute(bundle.clone(), database, None).await {
+                return Err(err);
+            }
+        }
+        Ok(())
     }
 }
