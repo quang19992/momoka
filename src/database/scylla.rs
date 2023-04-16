e222203f6977ae0b36ec69a1ef70d44767da772a
Quang1999 <contact@relay.akimitsu.dev>
2023-04-16 22:21:38 +0700

    fix: change `futures::executor::block_on` to `tokio::task::block_in_place`

--- src/database/scylla.rs
+++ src/database/scylla.rs
@@ -3,7 +3,7 @@ use super::{
     sync::{SyncResponse, SyncSupport},
 };
 use crate::server_config::scylla::ScyllaConfig;
-use scylla::{IntoTypedRows, Session, SessionBuilder};
+use scylla::{transport::errors, IntoTypedRows, Session, SessionBuilder};
 use std::result::Result;
 
 pub struct ScyllaWrapper {
@@ -29,10 +29,26 @@ impl ScyllaWrapper {
 impl SyncSupport for ScyllaWrapper {
     fn schema_version(&self) -> Result<Option<i64>, DatabaseError> {
         const VERSION_FIELD: &str = "schema_version";
-        const VERSION_QUERY: &str =
-            "SELECT value FROM sync_data WHERE field = ?;";
-        let query = self.session.query(VERSION_QUERY, vec![VERSION_FIELD]);
-        let query = futures::executor::block_on(query)?;
+        const VERSION_QUERY: &str = "SELECT value FROM sync_data WHERE field = ?;";
+        let query = tokio::task::block_in_place(move || {
+            tokio::runtime::Handle::current().block_on(async move {
+                self.session.query(VERSION_QUERY, vec![VERSION_FIELD]).await
+            })
+        });
+        let query = match query {
+            Err(err) => match err {
+                errors::QueryError::DbError(err, message) => match err {
+                    errors::DbError::Invalid => return Ok(None),
+                    _ => {
+                        return Err(DatabaseError::ScyllaQueryError(
+                            errors::QueryError::DbError(err, message),
+                        ))
+                    }
+                },
+                _ => return Err(DatabaseError::ScyllaQueryError(err)),
+            },
+            Ok(query) => query,
+        };
         let rows = match query.rows {
             Some(rows) => rows,
             None => return Ok(None),
@@ -49,7 +65,10 @@ impl SyncSupport for ScyllaWrapper {
     }
 
     fn execute(&self, query: &str) -> SyncResponse {
-        futures::executor::block_on(self.session.query(query, &[]))?;
+        tokio::task::block_in_place(move || {
+            tokio::runtime::Handle::current()
+                .block_on(async move { self.session.query(query, &[]).await })
+        })?;
         Ok(())
     }
 }
